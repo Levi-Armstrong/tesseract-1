54707b153cac0c4d44d495f097e2a3af4a4e1c92
Levi Armstrong <levi.armstrong@gmail.com>
2020-10-16 10:00:30 -0500

    fixup

--- tesseract/tesseract_scene_graph/include/tesseract_scene_graph/graph.h
+++ tesseract/tesseract_scene_graph/include/tesseract_scene_graph/graph.h
@@ -93,8 +93,14 @@ using VertexProperty = boost::property<
  */
 using EdgeProperty = boost::property<boost::edge_joint_t, Joint::Ptr, boost::property<boost::edge_weight_t, double>>;
 
+// This is just to get windows to build but is not a solution because hash_map invalidate iterators on add/delete
+#ifdef (MSVC)
 using Graph = boost::
     adjacency_list<boost::hash_mapS, boost::listS, boost::bidirectionalS, VertexProperty, EdgeProperty, GraphProperty>;
+#else(MSVC)
+using Graph = boost::
+    adjacency_list<boost::listS, boost::listS, boost::bidirectionalS, VertexProperty, EdgeProperty, GraphProperty>;
+#endif(MSVC)
 
 class TESSERACT_SCENE_GRAPH_PUBLIC SceneGraph : public Graph
 {
